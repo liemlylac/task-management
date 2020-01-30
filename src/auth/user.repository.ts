411b523f87001e1fbed1cd063d8068ca252e2c20
liemlylac <liemlylac@gmail.com>
2020-01-30 16:32:00 +0700

    Setting up AuthModule, User Entity and UserRepository

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,14 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { UserEntity } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@EntityRepository(UserEntity)
+export class UserRepository extends Repository<UserEntity> {
+    async signUp(authCredentialsDto: AuthCredentialsDto) {
+        const { username, password } = authCredentialsDto;
+        const user = new UserEntity();
+        user.username = username;
+        user.password = password;
+        await user.save();
+    }
+}
